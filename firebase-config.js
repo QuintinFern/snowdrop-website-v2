2823b0c50c8325a3ff41db800ddb15ebd0b26b59
QuintinFern <quintin@halo.science>
2025-12-17 22:03:11 -0600

    16

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// --- CORRECT IMPORTS FOR BROWSERS (DO NOT CHANGE THESE URLs) ---
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// --- YOUR CONFIGURATION ---
+// Go to Firebase Console > Project Settings > General > Your Apps to find these values
 const firebaseConfig = {
-  apiKey: "AIzaSyBzwB9rLvP7hdeUitznATJZTP8W_3J62Go",
+  apiKey: AIzaSyBzwB9rLvP7hdeUitznATJZTP8W_3J62Go",
   authDomain: "snowdrop-united-social-website.firebaseapp.com",
   projectId: "snowdrop-united-social-website",
   storageBucket: "snowdrop-united-social-website.firebasestorage.app",
   messagingSenderId: "497770313012",
-  appId: "1:497770313012:web:a049acc032bae59671e161",
-  measurementId: "G-S20EMD150E"
+  appId: "1:497770313012:web:a049acc032bae59671e161"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export tools so other files can use them
+export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, collection, addDoc, onSnapshot, query, orderBy, serverTimestamp };
